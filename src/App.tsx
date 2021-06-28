9ddc19286d4551933d647a57f8bab7f4008a311f
Drielison Lopes <drielisonl@gmail.com>
2021-06-27 22:09:15 -0300

    hook useRoom - like, remoção, sair

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,26 @@
 // em typescript
 //string[] == array<string>
 
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
 
 import { Home } from "./pages/Home";
 import { NewRoom } from "./pages/NewRoom";
+import { Room } from './pages/Room';
+import { AdminRoom } from './pages/AdminRoom';
 
-import { AuthContextProvider } from './contexts/AuthContext';
+import { AuthContextProvider } from './contexts/AuthContext'
 
 function App() {
   return (
     <BrowserRouter>
       <AuthContextProvider>
-        {/* O exact informa que a rota é exclusivamente com somente / */}
-        <Route path="/" exact component={Home} />
-        <Route path="/rooms/new" component={NewRoom} />
+        <Switch>
+          {/* O exact informa que a rota é exclusivamente com somente / */}
+          <Route path="/" exact component={Home} />
+          <Route path="/rooms/new" component={NewRoom} />
+          <Route path="/rooms/:id" component={Room} />
+          <Route path="/admin/rooms/:id" component={AdminRoom} />
+        </Switch>
       </AuthContextProvider>
     </BrowserRouter>
   );
